38503d11ad751b46793d82600694e0f8c58690ff
sanqingyo <3316590668@qq.com>
2025-03-25 14:02:35 +0800

    pose

---
+++ build/src/process/CMakeFiles/process.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/process.dir/process.cpp.obj"
+  "CMakeFiles/process.dir/process.cpp.obj.d"
+  "D:/Cmake/cmake_planning_demo/bin/libprocess.dll"
+  "D:/Cmake/cmake_planning_demo/bin/libprocess.dll.manifest"
+  "D:/Cmake/cmake_planning_demo/bin/libprocess.pdb"
+  "libprocess.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/process.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/process/process.h
+++ src/process/process.h
@@ -1,6 +1,5 @@
 #pragma once
 #include "pnc_map.h"
-
 class Process
 {
 public:
